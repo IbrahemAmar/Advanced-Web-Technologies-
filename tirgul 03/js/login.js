1a2ff4f53281ea8f90409b0aa854d6436504ee10
IbrahemAmar <abrahem.sadekk@gmail.com>
2025-11-24 21:52:30 +0200

    fetch & dynamic user management

--- tirgul 03/js/login.js
+++ tirgul 03/js/login.js
@@ -1,24 +1,33 @@
-function login(event) {
-    event.preventDefault(); // prevent page refresh
+const html = document.documentElement;
+const loginForm = document.getElementById('loginForm');
+const loginMessage = document.getElementById('loginMessage');
 
-    const usernameOrEmail = document.getElementById("userInput").value.trim();
-    const password = document.getElementById("passInput").value.trim();
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
 
-    // use the find() function from users.js
-    let user = find(usernameOrEmail);
+    const usernameOrEmail = document.getElementById('userInput').value;
+    const password = document.getElementById('passInput').value;
 
-    if (!user) {
-        alert("User not found");
-        return;
+    let nameOrEmail = 'test';
+    let pass = 'password';
+    
+    let user = find(usernameOrEmail)
+
+    if(user != undefined){
+        nameOrEmail = user.username;
+        pass = user.password;
     }
 
-    if (user.password !== password) {
-        alert("Wrong password");
-        return;
-    }
-
-    alert("Login successful!");
-
-    // optional: redirect to another page:
-    // window.location.href = "home.html";
-}
+    if (usernameOrEmail === nameOrEmail && password === pass) {
+        loginMessage.textContent = 'Login successful!';
+        loginMessage.classList.remove('text-red-500');
+        loginMessage.classList.add('text-green-500');
+        localStorage.setItem('currentUser', JSON.stringify(user));
+ 	    if(user.isAdmin == true)
+            window.open("userManagement.html", "_self");
+    } else {
+        loginMessage.textContent = 'Invalid username/email or password.';
+        loginMessage.classList.remove('text-green-500');
+        loginMessage.classList.add('text-red-500');
+    }
+});
